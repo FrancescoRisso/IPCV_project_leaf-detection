2ac95d6aea287867a726137afda945c32ccbba87
FrancescoRisso <francesco@frisso.net>
2024-11-04 21:07:44 +0100

    Added parameters from main to handle all the functionalities

--- clear_dataset_feature.py
+++ clear_dataset_feature.py
@@ -2,16 +2,13 @@ import os
 import json
 
 
-def clear_dataset_feature() -> None:
+def clear_dataset_feature(feature: str) -> None:
     for leaf in os.listdir("./dataset/descriptions"):
         for file_name in os.listdir(f"./dataset/descriptions/{leaf}"):
             with open(f"./dataset/descriptions/{leaf}/{file_name}", "r") as file:
                 cache = json.load(file)
 
-            # Delete here what you want to delete
-            # example: del cache["features"]["height"]
-            
-            # Done
+            del cache["features"][feature]
 
             with open(f"./dataset/descriptions/{leaf}/{file_name}", "w") as file:
                 json.dump(cache, file)
--- main.py
+++ main.py
@@ -1,17 +1,82 @@
-from update_dataset import update_dataset
-from functions.classifiers.bayes.summarize_dataset import BAYES_summarize_dataset
-from functions.classifiers.bayes.check_correlation import BAYES_check_correlation
-from functions.classifiers.bayes.classifier import BAYES_classify
-from functions.classifiers.result import print_classification_result
-from functions.features import ImageFeatures
+import argparse
+import sys
 import cv2
 
-if __name__ == "__main__":
-    # update_dataset()
-    # BAYES_summarize_dataset()
-    # BAYES_check_correlation()
+from update_dataset import update_dataset
+from clear_dataset_feature import clear_dataset_feature
+from functions.classifiers.bayes.classifier import BAYES_classify
+from functions.classifiers.bayes.check_correlation import BAYES_check_correlation
+from functions.classifiers.result import print_classification_result
+from functions.features import ImageFeatures
 
-    plant = "gaggia-data"
-    print(f'Programmer\'s knowledge: plant is "{plant}"\n')
-    f = ImageFeatures(cv2.imread(f"./test/{plant}.jpg"))
-    print_classification_result(BAYES_classify(f.get_features()))
+
+def args_def() -> tuple[argparse.ArgumentParser, dict[str, argparse.ArgumentParser]]:
+    args = argparse.ArgumentParser(prog="leaf_classifier")
+
+    subparsers = args.add_subparsers(
+        dest="command",
+    )
+
+    subparsers.add_parser(
+        name="update",
+        help="update the dataset after adding some images or features",
+    )
+
+    remove_feature = subparsers.add_parser(
+        name="rmfeature",
+        help="remove a feature from the cached values",
+    )
+    remove_feature.add_argument(
+        "--feature",
+        "-f",
+        type=str,
+        action="store",
+        help="the name of the feature to be removed (mandatory)",
+    )
+
+    classify = subparsers.add_parser(
+        name="classify",
+        help="perform the classification task on a picture",
+    )
+    classify.add_argument(
+        "--img",
+        "-i",
+        type=str,
+        action="store",
+        help="the path to the image to be classified (mandatory)",
+    )
+
+    subparsers.add_parser(
+        name="correlation",
+        help="show the correlation matrix for all the features",
+    )
+
+    return (args, {"rm": remove_feature, "c": classify})
+
+
+if __name__ == "__main__":
+    args_parser, subparsers = args_def()
+    args = args_parser.parse_args(sys.argv[1:])
+
+    if args.command == "update":
+        update_dataset()
+
+    elif args.command == "rmfeature":
+        if args.feature == None:
+            subparsers["rm"].print_help()
+        else:
+            clear_dataset_feature(args.feature)
+
+    elif args.command == "classify":
+        if args.img == None:
+            subparsers["c"].print_help()
+        else:
+            print("Starting analizing picture...")
+            img = ImageFeatures(cv2.imread(args.img))
+            print_classification_result(BAYES_classify(img.get_features()))
+
+    elif args.command == "correlation":
+        BAYES_check_correlation()
+
+    else:
+        args_parser.print_help()
