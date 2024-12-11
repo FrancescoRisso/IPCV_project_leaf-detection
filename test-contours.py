a3aa99026e3fb526f8a410d2eb7d3bc036834020
EliaV <evige@outlook.it>
2024-12-11 22:51:02 +0100

    Added some comments

--- test-contours.py
+++ test-contours.py
@@ -16,55 +16,73 @@ l, r, t, b = find_roi_boundaries(img)
 img = img[t:b, l:r]
 mask = get_leaf_mask(cv2.cvtColor(img, cv2.COLOR_BGR2HSV))
 
-ker1 = np.ones((56, 56), np.uint8)
-ker2 = np.ones((6,6), np.uint8)
-ker3 = np.ones((2,2), np.uint8)
-kerR  = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(104, 104))
+def find_leaf_contour(mask: MatLike) -> np.ndarray:
+    """
+    The function retrives the leaf contours using openCV findContours
+    function, after applying a series of morphological operation to 
+    find the right contour
 
-mask = cv2.dilate(mask, kerR)
-mask = cv2.erode(mask, kerR)
+    ---------------------------------------------------------------------
+    PARAMETERS
+    ----------
 
-# noise cleanup
-mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((12,12)))
+    - leafMask: the tresholded image of the leaf, a bitmap
 
-# add a 10px black border around the image, so the leaves that exceed the image
-# dimensions are properly elaborated by Canny and then by the findContour function
-mask = cv2.copyMakeBorder(mask, 10, 10, 10, 10, cv2.BORDER_CONSTANT, value=0)
+    ---------------------------------------------------------------------
+    OUTPUT
+    ------
+    The main contour of the leaf, as a numpy array.
+    It is accepted by the openCV functions which work with contours 
+    """
 
-        #
-        # WARNING: POSSIBLE COORDINATES PROBLEM WITH THAT APPROACH
-        #
+    # a huge closing takes place, using a circular kernel. It is a necessary
+    # passage, all the holes in the leaves are filled to avoid problems
+    # like distinguish between different contours
+    ker2 = np.ones((6,6), np.uint8)
+    ker3 = np.ones((2,2), np.uint8)
+    kerR  = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(104, 104))
 
-edge = cv2.Canny(mask, 175, 175)
-edge = cv2.dilate(edge, ker2)   
-edge = cv2.erode(edge, ker2)
-edge = cv2.dilate(edge, ker3)  
+    mask = cv2.dilate(mask, kerR)
+    mask = cv2.erode(mask, kerR)
 
-contours, hierarchy = cv2.findContours(edge, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-# contours is a list of all the detected contours, where each one of them is an array of point representing it
-# hierarchy
+    # noise cleanup
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((12,12)))
 
-bestCnt = None
-if len(contours) > 1 :
-    bestArea = 0
-    for cont in contours:
-        areaC = cv2.contourArea(cont)
-        if areaC > bestArea:
-            bestCnt = cont
-    contour = bestCnt
-else:
-    contour = contours[0]
+    # add a 10px black border around the image, so the leaves which exceed the image
+    # dimensions are properly elaborated by Canny and then by the findContour function
+    mask = cv2.copyMakeBorder(mask, 10, 10, 10, 10, cv2.BORDER_CONSTANT, value=0)
 
+    # N.B. the new coordinates are not usable on th eleaf maske as the image is bigger,
+    #       however the features which will be extracted are not position related so we'll ignore that 
 
-#cnt = contours[4]
-#cv2.drawContours(img, [cnt], 0, (0,255,0), 5)
-cv2.drawContours(img, contour, -1, (0,0,255), 8)
+    edge = cv2.Canny(mask, 175, 175)
+    edge = cv2.dilate(edge, ker2)   
+    edge = cv2.erode(edge, ker2)
+    edge = cv2.dilate(edge, ker3)  
+
+    contours, hierarchy = cv2.findContours(edge, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # contours is a list of all the detected contours
+
+    # select the right contour by finding the biggest one, the one which contains the most pixels
+    bestCnt = None
+    if len(contours) > 1 :
+        bestArea = 0
+        for cont in contours:
+            areaC = cv2.contourArea(cont)
+            if areaC > bestArea:
+                bestCnt = cont
+        contour = bestCnt
+    else:
+        contour = contours[0]
+
+    return contour
 
 ###
 #
 #   FEATURE EXTRACTION
 #
 ###
+contour = find_leaf_contour(mask)
 
 perimeter = cv2.arcLength(contour, False)
 
@@ -89,17 +107,75 @@ print(" - hull: ", areaHull, " - cntArea: ",areaContour, " - diff: ",areaHull-ar
 
 
 
-plt.subplot(1,3,1)
+plt.subplot(1,2,1)
 plt.imshow(mask, cmap="grey")
 plt.axis("off")
 
-plt.subplot(1,3,2)
-plt.imshow(edge, cmap="grey")
-plt.axis("off")
 
-plt.subplot(1,3,3)
+plt.subplot(1,2,2)
 plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 plt.axis("off")
 
 
 plt.show()
+
+def get_leaf_contours(img: MatLike) -> Tuple[float] :
+    """
+    The function retrives the leaf contours using openCV findContours
+    function, after applying a series of morphological operation to 
+    find the right contour
+
+    ---------------------------------------------------------------------
+    PARAMETERS
+    ----------
+
+    - leafMask: the tresholded image of the leaf, a bitmap
+
+    ---------------------------------------------------------------------
+    OUTPUT
+    ------
+    A tuple, composed of:
+    - The main contour of the leaf, usable by other openCV functions
+    """
+    ker1 = np.ones((56, 56), np.uint8)
+    ker2 = np.ones((6,6), np.uint8)
+    ker3 = np.ones((2,2), np.uint8)
+    kerR  = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(104, 104))
+
+    mask = cv2.dilate(mask, kerR)
+    mask = cv2.erode(mask, kerR)
+
+    # noise cleanup
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((12,12)))
+
+    # add a 10px black border around the image, so the leaves that exceed the image
+    # dimensions are properly elaborated by Canny and then by the findContour function
+    mask = cv2.copyMakeBorder(mask, 10, 10, 10, 10, cv2.BORDER_CONSTANT, value=0)
+
+            #
+            # WARNING: POSSIBLE COORDINATES PROBLEM WITH THAT APPROACH
+            #
+
+    edge = cv2.Canny(mask, 175, 175)
+    edge = cv2.dilate(edge, ker2)   
+    edge = cv2.erode(edge, ker2)
+    edge = cv2.dilate(edge, ker3)  
+
+    contours, hierarchy = cv2.findContours(edge, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # contours is a list of all the detected contours, where each one of them is an array of point representing it
+    # hierarchy
+
+    bestCnt = None
+    if len(contours) > 1 :
+        bestArea = 0
+        for cont in contours:
+            areaC = cv2.contourArea(cont)
+            if areaC > bestArea:
+                bestCnt = cont
+        contour = bestCnt
+    else:
+        contour = contours[0]
+
+
+
+    return
