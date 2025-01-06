06075d5a6b56d3996e57d8d2cd7bf2ee504cdadb
FrancescoRisso <francesco@frisso.net>
2025-01-06 17:56:40 +0100

    Added option to print classification result in a (non) verbose way

--- functions/classifiers/result.py
+++ functions/classifiers/result.py
@@ -1,4 +1,4 @@
-def print_classification_result(perc: dict[str, float]) -> None:
+def print_classification_result(perc: dict[str, float], verbose: bool) -> None:
     """
     Prints the result of a classification task in a tidy way
 
@@ -20,6 +20,7 @@ def print_classification_result(perc: dict[str, float]) -> None:
         sum += val
 
     print(f'Classified as "{argmax}" with confidence {(max/sum*100):2.4f}%')
-    print("Full classification result:")
-    for leaf, val in reversed(sorted(perc.items(), key=lambda x: x[1])):
-        print(f"- {(val/sum*100):8.4f}% --> {leaf}")
+    if verbose:
+        print("Full classification result:")
+        for leaf, val in reversed(sorted(perc.items(), key=lambda x: x[1])):
+            print(f"- {(val/sum*100):8.4f}% --> {leaf}")
